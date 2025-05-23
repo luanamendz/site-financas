b8990ee248ed5ed69139a3996039222a60698d46
luanamendz <luana.mendes.ferr@gmail.com>
2025-05-23 11:05:59 -0300

    pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Gerador de Planilhas Financeiras!</h1>
+      <p>Aqui você controla seus gastos de forma simples e gratuita.</p>
+    </div>
+  );
+}
